a79a585d021cf907984999427fa3b6a9a346f2e8
Vaishnavi Manikeri <vaishnavimanikeri@gmail.com>
2026-01-31 13:25:05 +0530

    Fix CORS to allow Authorization header for delete API

--- server.js
+++ server.js
@@ -17,8 +17,10 @@ app.use(
   cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
